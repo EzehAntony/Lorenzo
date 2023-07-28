3cd7a87d80464b8d5d102d895c659c43b16091b3
Anthony <crayonno.o@gmail.com>
2023-07-28 03:37:07 +0100

    Categories

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -3,17 +3,17 @@ function Categories() {
     <>
       <h3 className="text-lg mt-14 mb-4 font-bold">Categories</h3>
       <div className="w-[100%] px-4 flex justify-around items-center">
-        <div className="max-w-[150px] min-h-[150px] p-[10px] flex flex-col justify-around items-start">
+        <div className="max-w-[200px] min-h-[150px] p-[10px] flex flex-col justify-around items-start">
           <img
-            src="man.webp"
+            src="man.jpg"
             className="w-[150px] max-h-[150px] object-cover"
             alt=""
           />
           <p className="my-2">Men</p>
         </div>
-        <div className="max-w-[150px] min-h-[150px] p-[10px] flex flex-col justify-around items-start ">
+        <div className="max-w-[200px] min-h-[150px] p-[10px] flex flex-col justify-around items-start ">
           <img
-            src="woman.webp"
+            src="woman.jpg"
             className="w-[150px]  max-h-[150px] object-cover"
             alt=""
           />
