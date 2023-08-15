2ba504fe62d4f7d406afac0b4c7ecb1e9ca2ec53
Anthony <crayonno.o@gmail.com>
2023-08-15 01:58:26 +0100

    window fix

--- app/category/page.jsx
+++ app/category/page.jsx
@@ -4,10 +4,6 @@ import { useEffect } from "react";
 import Header from "@/components/Header";
 
 const page = () => {
-  useEffect(() => {
-    window.scrollY > 100 ? console.log(true) : console.log(false);
-  }, [window.scrolly]);
-
   return (
     <div className="min-h-[2000px]">
       <Header />
